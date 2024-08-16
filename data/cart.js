6ae6d6e13a57412b3a0d3042ee0850ecb7210aa3
cmanh75 <npcm752004t2k29@gmail.com>
2024-08-16 23:00:37 +0700

    Generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
 export const cart = JSON.parse(localStorage.getItem('cart')) || [
 {
   id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '1'
 },
 {
   id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '2'
 }
 ];
 
@@ -22,7 +24,8 @@ export function addProduct(productId) {
   if (!itemInCart) {
     cart.push({
       id: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
     cartQuantity++;
   }
